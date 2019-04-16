a734f062e0503a8d7783e732b3238408968a7ff9
Harry <411981379@qq.com>
2019-04-16 08:46:11 +0000

    manul mode

--- LEDStateMachine.js
+++ LEDStateMachine.js
@@ -27,15 +27,205 @@ class Init extends State {
   enter() {
     this.ctx.i2c1 = i2c.openSync(BUS_NUMBER)
     this.ctx.set(0x00, 0x55) // reboot
+    this.ctx.set(0x01, 0x01) // reboot
     this.ctx.set(0x03, 0x00) // max brightness
-    this.ctx.set(0x07, 0x07
-      ) // enable all 
+    this.ctx.set(0x07, 0x07) // enable all 
     this.ctx.set(0x08, 0x08) // require in multi color mode
     this.ctx.setPWMS(0x55, 0x55, 0x55)
+    this.setState(StandBy)
   }
 }
 
-class StandBy extends State {
+class StandBy extends State{
+  constructor(ctx) {
+    super(ctx)
+  }
+
+  enter() {
+    this.ctx.setLedMode(0x06)
+    this.ctx.setLed1(0x00, 0xff, 0x00)
+    this.ctx.setPWMS(0X55)
+  }
+}
+
+class Working extends State {
+  constructor(ctx, ...args) {
+    super(ctx, ...args)
+    this.light = false
+    this.color = null
+    this.type = null
+    this.time = 0
+    this.times = 0
+    this.lightTimes = 0
+    this.closeTimer = null
+    this.nextTimer = null
+
+  }
+
+  enter(color, type, time = 0, times = 0) {
+    console.log(time)
+    let [r, g, b] = convertColor(color)
+    this.color = color
+    this.type = type
+    this.time = time
+    this.times = times
+    this.ctx.setLedMode(0x06)
+    this.ctx.setLed1(r, g, b)
+    this.ctx.setPWMS()
+    this.ctx.setTRiseAndOn(0x33)
+    this.ctx.setTFallAndOff(0x33)
+
+    if (type !== 'alwaysOn') this.createNextTimer()
+    if (time) this.closeTimer = setTimeout(() => {
+      console.log('in close timer')
+      this.setState(StandBy)
+    }, time)
+
+  }
+
+  exit() {
+    clearTimeout(this.nextTimer)
+    clearTimeout(this.closeTimer)
+  }
+
+  createNextTimer () {
+    this.nextTimer = setTimeout(() => {
+      // console.log('in next timer', this.lightTimes)
+      this.ctx.setPWMS(this.light? null: 0x55)
+      if (!this.light) this.lightTimes ++
+      this.light = !this.light
+      this.createNextTimer()
+    }, 500)
+  }
+}
+
+
+
+class LEDControl {
+  constructor() {
+    this.queue = []
+    this.i2c1 = null
+    this.state = null
+    this.addr = AW2015FCR_ADDR
+    this.busNumber = BUS_NUMBER
+    new Init(this)
+  }
+
+  set(cmd, byte) {
+    if (!this.i2c1) throw new Error('Not initialized yet')
+    // console.log(cmd, ' ', byte)
+    this.i2c1.writeByteSync(this.addr, cmd, byte)
+  }
+
+  get(cmd, num) {
+    if (!this.i2c1) throw new Error('Not initialized yet')
+    let result =  this.i2c1.readByteSync(this.addr, cmd)
+    return `${!num?'0x':''}${result.toString(num || 16)}`
+  }
+
+  setLedMode (value) {
+    // 0x07 pattern mode
+    // 0x06 manual mode
+    this.set(0x04, value)
+    this.set(0x05, value)
+    this.set(0x06, value)
+  }
+
+  setLed1(r, g, b) {
+    this.set(0x10, r)
+    this.set(0x11, g)
+    this.set(0x12, b)
+  }
+  
+  setLed([r1, r2, r3, r4], [g1, g2, g3, g4], [b1, b2, b3, b4]) {
+    this.set(0x10, r1 || 0x00)
+    this.set(0x13, r2 || 0x00)
+    this.set(0x16, r3 || 0x00)
+    this.set(0x19, r4 || 0x00)  
+    this.set(0x11, g1 || 0x00)
+    this.set(0x14, g2 || 0x00)
+    this.set(0x17, g3 || 0x00)
+    this.set(0x1A, g4 || 0x00)  
+    this.set(0x12, b1 || 0x00)
+    this.set(0x15, b2 || 0x00)
+    this.set(0x18, b3 || 0x00)
+    this.set(0x1B, b4 || 0x00)  
+  }
+  
+  // PWMs
+  setPWMS (l1, l2, l3) {
+    this.set(0x1C, l1 || 0x00)
+    this.set(0x1D, l2 || 0x00)
+    this.set(0x1E, l3 || 0x00)
+  }
+  
+  setTRiseAndOn(v1, v2, v3) {
+    this.set(0x30, v1 || 0x00)
+    this.set(0x35, v2 || 0x00)
+    this.set(0x3A, v3 || 0x00)
+  }
+  
+  setTFallAndOff(v1, v2, v3) {
+    this.set(0x31, v1 || 0x00)
+    this.set(0x36, v2 || 0x00)
+    this.set(0x3B, v3 || 0x00)
+  }
+  
+  setTSlotAndDelay(v1, v2, v3) {
+    this.set(0x32, v1 || 0x00)
+    this.set(0x37, v2 || 0x00)
+    this.set(0x3C, v3 || 0x00)
+  }
+
+  setPattern(v1, v2, v3) {
+    this.set(0x33, v1 || 0x00)
+    this.set(0x38, v2 || 0x00)
+    this.set(0x3D, v3 || 0x00)
+  }
+
+  setTimes(v1, v2, v3) {
+    this.set(0x34, v1 || 0x00)
+    this.set(0x39, v2 || 0x00)
+    this.set(0x3E, v3 || 0x00)
+  }
+
+  run(color, type, time, times) {
+    if (!['alwaysOn', 'breath'].includes(type)) throw new Error('illegal type')
+    if (time && typeof time !== 'number') throw new Error('illegal time')
+    if (times && typeof times !== 'number') throw new Error('illegal times')
+    this.state.setState(Working, color, type, time, times)
+  }
+
+  stop() {
+    // this.set(0x09, 0x10)
+  }
+}
+
+function convertColor(color) {
+  let match = color.match(/^(#[0-9a-fA-F]{6}){1}$/g)
+  if (!match) throw new Error('color is illegal')
+  return [
+    parseHex(color.substring(1,3)), 
+    parseHex(color.substring(3,5)), 
+    parseHex(color.substring(5,7))
+  ]
+}
+
+function parseHex(number) {
+  return parseInt(number, 16)
+}
+
+let m = new LEDControl()
+
+// 常亮
+// m.run('#ffffff', 'alwaysOn')
+// 闪烁 5秒
+m.run('#ffffff', 'breath')
+// 闪烁 3次
+// m.run('#0000ff', 'breath', null, 2)
+
+
+/* class StandBy extends State {
   constructor(ctx) {
     super(ctx)
   }
@@ -128,126 +318,6 @@ class Err extends State {
     super(ctx)
   }
 }
+*/
 
-class LEDControl {
-  constructor() {
-    this.queue = []
-    this.i2c1 = null
-    this.state = null
-    this.addr = AW2015FCR_ADDR
-    this.busNumber = BUS_NUMBER
-    new Init(this)
-  }
 
-  set(cmd, byte) {
-    if (!this.i2c1) throw new Error('Not initialized yet')
-    // console.log(cmd, ' ', byte)
-    this.i2c1.writeByteSync(this.addr, cmd, byte)
-  }
-
-  get(cmd, num) {
-    if (!this.i2c1) throw new Error('Not initialized yet')
-    let result =  this.i2c1.readByteSync(this.addr, cmd)
-    return `${!num?'0x':''}${result.toString(num || 16)}`
-  }
-
-  setLedMode (value) {
-    // 0x07 pattern mode
-    // 0x06 manual mode
-    this.set(0x04, value)
-    this.set(0x05, value)
-    this.set(0x06, value)
-  }
-
-  setLed1(r, g, b) {
-    this.set(0x10, r)
-    this.set(0x11, g)
-    this.set(0x12, b)
-  }
-  
-  setLed([r1, r2, r3, r4], [g1, g2, g3, g4], [b1, b2, b3, b4]) {
-    this.set(0x10, r1 || 0x00)
-    this.set(0x13, r2 || 0x00)
-    this.set(0x16, r3 || 0x00)
-    this.set(0x19, r4 || 0x00)  
-    this.set(0x11, g1 || 0x00)
-    this.set(0x14, g2 || 0x00)
-    this.set(0x17, g3 || 0x00)
-    this.set(0x1A, g4 || 0x00)  
-    this.set(0x12, b1 || 0x00)
-    this.set(0x15, b2 || 0x00)
-    this.set(0x18, b3 || 0x00)
-    this.set(0x1B, b4 || 0x00)  
-  }
-  
-  // PWMs
-  setPWMS (l1, l2, l3) {
-    this.set(0x1C, l1)
-    this.set(0x1D, l2)
-    this.set(0x1E, l3)
-  }
-  
-  setTRiseAndOn(v1, v2, v3) {
-    this.set(0x30, v1 || 0x00)
-    this.set(0x35, v2 || 0x00)
-    this.set(0x3A, v3 || 0x00)
-  }
-  
-  setTFallAndOff(v1, v2, v3) {
-    this.set(0x31, v1 || 0x00)
-    this.set(0x36, v2 || 0x00)
-    this.set(0x3B, v3 || 0x00)
-  }
-  
-  setTSlotAndDelay(v1, v2, v3) {
-    this.set(0x32, v1 || 0x00)
-    this.set(0x37, v2 || 0x00)
-    this.set(0x3C, v3 || 0x00)
-  }
-
-  setPattern(v1, v2, v3) {
-    this.set(0x33, v1 || 0x00)
-    this.set(0x38, v2 || 0x00)
-    this.set(0x3D, v3 || 0x00)
-  }
-
-  setTimes(v1, v2, v3) {
-    this.set(0x34, v1 || 0x00)
-    this.set(0x39, v2 || 0x00)
-    this.set(0x3E, v3 || 0x00)
-  }
-
-  run(color, type, time, times) {
-    if (!['alwaysOn', 'breath'].includes(type)) throw new Error('illegal type')
-    if (time && typeof time !== 'number') throw new Error('illegal time')
-    if (times && typeof times !== 'number') throw new Error('illegal times')
-    this.state.setState(Working, color, type, time, times)
-  }
-
-  stop() {
-    // this.set(0x09, 0x10)
-  }
-}
-
-function convertColor(color) {
-  let match = color.match(/^(#[0-9a-fA-F]{6}){1}$/g)
-  if (!match) throw new Error('color is illegal')
-  return [
-    parseHex(color.substring(1,3)), 
-    parseHex(color.substring(3,5)), 
-    parseHex(color.substring(5,7))
-  ]
-}
-
-function parseHex(number) {
-  return parseInt(number, 16)
-}
-
-let m = new LEDControl()
-
-// 常亮
-// m.run('#ffffff', 'alwaysOn')
-// 闪烁 5秒
-m.run('#00ff00', 'breath')
-// 闪烁 3次
-// m.run('#0000ff', 'breath', null, 2)
